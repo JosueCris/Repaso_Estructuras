b7abc90ed7ecd9495cdb51ffde50d1ab5f773aef
Josue Téllez <josuetllezh@gmail.com>
2023-01-04 23:20:12 -0600

    Pila estatica actualizada

--- Estructuras/Basicas/Pila_Estatica.c
+++ Estructuras/Basicas/Pila_Estatica.c
@@ -14,66 +14,119 @@ struct Pila_Estatica
 
 typedef struct Pila_Estatica Stack;
 
+void menu();
+int length(Stack *start);
+
 void stack_begin(Stack *start);
-void stack_free(Stack *start);
 int is_empty(Stack *start);
 int is_full(Stack *start);
 int push(Stack *start, int N);
 int pop(Stack *start);
 void seek(Stack *start);
+void stack_free(Stack *start);
 
 
 int main(int argc, char const *argv[])
 {
     Stack stack;
+    int option, number, buscar;
 
     stack_begin(&stack);
-    printf("Longitud pila: %d \n", stack.index);
-    int num = 1;
-    while (!is_full(&stack))
-    {
-        push(&stack, num);
-        num++;
-    }
-    push(&stack, 9);
-    
-    // push(&stack, 1);
-    // push(&stack, 2);
-    // push(&stack, 3);
-    // push(&stack, 4);
-    // push(&stack, 5);
 
-    printf("PILA: \n");
-    seek(&stack);    
-    printf("Longitud pila: %d \n", stack.index);
-
-    // for (int i=0; i<size; i++)
-    //     printf("%d \n", pop(&stack));
-    printf("QUITANDO . . . \n");
-    while(!is_empty(&stack))
+    do
 	{
-		printf("%d \n", pop(&stack));
-	}
-    pop(&stack);
+		menu();
+		scanf("%d", &option);
 
-    printf("PILA: \n");
-    seek(&stack);
-    printf("Longitud pila: %d \n", stack.index);
+		switch(option)
+		{
+			case 1:
+				printf("Insert the number in the stack: ");
+				scanf("%d", &number);
+				if (push(&stack, number))
+                    printf("Inserted \n\n");
+                else
+                    printf("No inserted!!! \n\n");
+				break;
+
+			case 2:
+				printf("\nSTACK: \n");
+				seek(&stack);
+				printf("\n\n");
+				break;		
+			
+			case 3: 
+// 				printf("Insert the number to search: ");
+// 				scanf("%d", &number);
+// //				Stack *item = search(&stack, NULL, number);
+// 				search(&stack, NULL, number);
+// /*				if (item)
+// 					printf("Founded: %d \n\n", item->data);
+// 				else
+// 					printf("No Exist!!!\n\n");
+// */
+// 				break;
+
+			case 4:
+				if (pop(&stack))
+                    printf("Deleted \n\n");
+                else
+                    printf("No delete!!! \n\n");
+				break;
+
+			case 0:
+				printf("\nSee you later :D \n\n");
+				return 0;
+				break;	
+
+			default:
+				printf("Try Again!!!");
+				break;	
+		}
+	}while(option != 0);
+
+    // printf("Longitud pila: %d \n", stack.index);
+    // int num = 1;
+    // while (!is_full(&stack))
+    // {
+    //     push(&stack, num);
+    //     num++;
+    // }
+    // push(&stack, 9);
+
+    // printf("PILA: \n");
+    // seek(&stack);    
+    // printf("Longitud pila: %d \n", stack.index);
+
+    // printf("QUITANDO . . . \n");
+    // while(!is_empty(&stack))
+	// {
+	// 	printf("%d \n", pop(&stack));
+	// }
+    // pop(&stack);
+
+    // printf("PILA: \n");
+    // seek(&stack);
+    // printf("Longitud pila: %d \n", stack.index);
 
 
     return 0;
 }
 
+void menu()
+{
+	printf("Select your option to choice from the stack: \n\t");
+	printf("[1]: Insert\n\t[2]: Show\n\t[3]: Search\n\t[4]: Delete\n\t[0]: Exit\n> ");
+}
+
+int length(Stack *start)
+{
+    return start->index;
+}
+
 void stack_begin(Stack *start)
 {
 	start->index = 0;
-}
-
-void stack_free(Stack *start)
-{
-    while (!is_empty(start))
-        printf("%d \n", pop(start));
-    
 }
 
 int is_empty(Stack *start)
@@ -119,4 +172,11 @@ void seek(Stack *start)
 {
     for(int i=start->index-1; i>-1; i--)
 		printf("%d \n", start->storage[i]);
+}
+
+void stack_free(Stack *start)
+{
+    while (!is_empty(start))
+        printf("%d \n", pop(start));
+    
 }
